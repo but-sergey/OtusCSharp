4e999f34ab060bfc61de542fd8b12ef3e1002936
but-sergey <but-sergey@bk.ru>
2020-06-16 13:29:58 +0300

    Код задачи №1

---
+++ Lesson03/Task01/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task01
+{
+    class Program
+    {
+        public void Show()
+        {
+            A a = new A(), b = new B(), c = new C();
+            I ia = new A(), ib = new B(), ic = new C();
+            Console.WriteLine($"{a.P}, {b.P}, {c.P}, {ia.P}, {ib.P}, {ic.P}");
+        }
+
+        interface I
+        {
+            int P { get; }
+        }
+
+        class A : I
+        {
+            public virtual int P => 0;
+        }
+
+        class B : A
+        {
+            public override int P => 1;
+        }
+
+        class C : B, I
+        {
+            public int P => 2;
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Show();
+
+            Console.ReadKey();
+        }
+    }
+}
