6cac9dbd61053a3393acc0a8e48b8b8ce7dbecdf
but-sergey <but-sergey@bk.ru>
2020-06-16 09:49:02 +0300

    Initial commit. Создана заготовка проекта

---
+++ Lesson03/Lesson03-1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Lesson03_1
+{
+    public struct Point
+    {
+        public double X, Y;
+    }
+
+    public interface IFigure
+    {
+        List<Point> Points { get; }
+        double GetPerimeter();
+    }
+
+    public abstract class Figure : IFigure { }
+
+    public class Round : Figure { }
+
+    public class Triangle : Figure { }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
