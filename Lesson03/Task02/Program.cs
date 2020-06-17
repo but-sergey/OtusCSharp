63068bd920af2ea33d24514a9a51e27a7bfc95bc
but-sergey <but-sergey@bk.ru>
2020-06-17 12:39:33 +0300

    Задачи 2 и 3

---
+++ Lesson03/Task02/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task02
+{
+    class Program
+    {
+        public void Show()
+        {
+            A a = new B();
+        }
+
+        public class A
+        {
+            public A()
+            {
+                Console.WriteLine("Constructor A called");
+                Console.WriteLine($"B value = {B.value}");
+            }
+        }
+
+        public class B : A
+        {
+            public B() => Console.WriteLine("Constructor B called");
+            static B()
+            {
+                value = 0;
+                Console.WriteLine("Static constructor B called");
+            }
+            public static int value = 1;
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Show();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson03/Task03/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task03
+{
+    class Program
+    {
+        class A
+        {
+            public virtual void Foo() => Console.WriteLine("A");
+        }
+
+        class B : A
+        {
+            public override void Foo() => Console.WriteLine("B");
+        }
+
+        class C : A
+        {
+            public new void Foo() => Console.WriteLine("C");
+        }
+
+        class D : A
+        {
+            public void Foo() => Console.WriteLine("D");
+        }
+
+        class E : B
+        {
+            public new void Foo() => Console.WriteLine("E");
+        }
+
+        static void Main(string[] args)
+        {
+            //A a = new A();
+            //a.Foo();
+
+            //B b1 = new A();
+            //b1.Foo();
+
+            //A b2 = new B();
+            //b2.Foo();
+
+            //A c = new C();
+            //c.Foo();
+
+            //B b3 = new C();
+            //b3.Foo();
+
+            //D d = new D();
+            //d.Foo();
+
+            E e = new E();
+            e.Foo();
+
+            Console.ReadKey();
+        }
+    }
+}
