dc36101853717c684d1c4974c373aa87c5255187
but-sergey <but-sergey@bk.ru>
2020-11-06 15:23:44 +0300

    Основы интерфейсов

---
+++ Lesson04/lesson04/ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    interface ISample
+    {
+        void SampleMethod();
+    }
+
+    interface ISample2
+    {
+        void SampleMethod2();
+    }
+
+    class Foo : ISample
+    {
+        public void SampleMethod()
+        {
+            Console.WriteLine("Вызван метод");
+        }
+    }
+
+    class Foo2:ISample, ISample2
+    {
+        public void SampleMethod()
+        {
+            Console.WriteLine("Вызван метод");
+        }
+
+        public void SampleMethod2()
+        {
+            Console.WriteLine("Вызван метод 2");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //error
+            //ISample sample = new ISample();
+            ISample foo = new Foo();
+            foo.SampleMethod();
+
+            Foo2 foo2 = new Foo2();
+            ISample sample = foo2;
+            sample.SampleMethod();
+
+            ISample2 sample2 = foo2;
+            sample2.SampleMethod2();
+        }
+    }
+}
